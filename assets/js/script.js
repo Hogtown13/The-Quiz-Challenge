809d5ffb4a8a857d46d60276517760745b48b03a
Hogtown13 <tribenation13@yahoo.com>
2022-04-28 20:42:20 -0400

    added local storage and high score event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,17 @@
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
 var startButton = document.getElementById('btn-start');
+var highScore = document.getElementById('btn-high');
 var quizCon = document.getElementById('question-container');
-var timerEl = document.getElementById('timer')
+var timerEl = document.getElementById('timer');
 var time = 100;
 var timerId;
 var questionIndex = 0;
-var questionEl = document.getElementById('question-text')
-var choicesCon = document.getElementById('choices')
-
+var questionEl = document.getElementById('question-text');
+var choicesCon = document.getElementById('choices');
+var submitCon = document.getElementById('submit');
+var submitBtn = document.getElementById('submit-btn')
+var initialInput = document.getElementById('initials')
 // TODO: Create questions and answers array
 var questions = [
     {
@@ -42,25 +45,23 @@ function timeTick(){
     timerEl.textContent = time
     if (time === 0)
     {
-        
+        endGame()
     };
 
 // -- reduces the time by an increment using setInterval method
 // -- if time === 0 then end quiz
 }
-
-
 // TODO: Create Start Quiz function
 function startQuiz(){
     timerId = setInterval(timeTick, 1000)
     timerEl.textContent = time
     startButton.setAttribute('class', 'hide');
     quizCon.removeAttribute('class');
+    highScore.setAttribute('class', 'hide');
     getQuestions();
 
 };
 // -- get the first question 
-
 function getQuestions(){
     var currentQuestion = questions[questionIndex];
     questionEl.textContent = currentQuestion.question;
@@ -80,18 +81,68 @@ function choiceClick(){
         timerEl.textContent = time
     }
     // End game function ***** START HERE*****
-    /* if(questionIndex)*/
-     questionIndex ++ 
-    getQuestions()
+    questionIndex++ 
+    if(questionIndex  === questions.length) {
+        endGame()
+    } else {
+        getQuestions()
+    }
 }
+function endGame() {
+    quizCon.setAttribute('class', 'hide');
+    submitCon.removeAttribute('class');
+    highScore.removeAttribute('class');// Cant get highscore to show with style???????
+    clearInterval(timerId);
+    
+};
+function submitScore(){
+    // gets our input initials
+    var scores = localStorage
+    
+        
+    
+
+
+    // initials and time go into an Object to be pushed into the array
+    localStorage.setItem('initials', 'timer')
+    // checks for localstorage to get previous initials 
+
+    // if there is localstorage push into that 
+    // if there isnt push into empty array then setItem to localstorage
+    // should use JSON.stringify when using setItem
+    
+    
+
+}
+
+function getScores(){
+    
+    // hides submit scores and other containers
+    submitBtn.removeAttribute('class', 'hide');
+    initialInput.removeAttribute('class', 'hide');
+    // check localstorage using getItem 
+    let initials = localStorage.getItem(initials);
+    let timer = localStorage.getItem(timer);
+    // if there is localstorage use foreach to create scores
+    // if there isnt any localstorage Just display 
+};
 
 startButton.addEventListener('click', function(e){
 e.preventDefault()
 startQuiz()
 });
-// -- start interval for time function 
-// -- unhide quizCon using remove attribute method
+submitBtn.addEventListener('submit', function(e){
+e.preventDefault()
 
+});
+submitBtn.addEventListener('click', function(e){
+    e.preventDefault()
+    submitScore()
+});
+/*highScore.addEventListener('click', function(e){
+    e.preventDefault()
+    getScores()
+} );*/
 
 
 // WHEN I answer a question
